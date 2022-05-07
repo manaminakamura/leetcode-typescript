7557408aeb23ee1218b5a0a1be6eb1ff5b1d1321
manaminakamura <manaminakamura812@gmail.com>
2022-05-08 01:21:02 +0900

    binary search

---
+++ binary-search.ts
@@ -0,0 +1,14 @@
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    while(left <= right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if(target === nums[mid]) {
+            return mid;
+        } else if(target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+};
